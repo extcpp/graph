e61a87ef1ced7715cfb1c5731ce830932ec47fa0
Jan Christoph Uhde <Jan@UhdeJC.com>
2020-06-03 17:46:58 +0200

    function based traits

--- include/ext/property_map/property_map.hpp
+++ include/ext/property_map/property_map.hpp
@@ -13,66 +13,76 @@
 
 namespace ext { namespace graph {
 
-    template<typename T>
-    struct property_map_trait {
-        using value_type = typename T::value_type;
-        using key_type = typename T::key_type;
-        using reference = typename T::reference;
-    };
+template<typename ValueType, typename KeyType, typename Reference>
+struct property_map_types {
+    using value_type = ValueType;
+    using key_type = KeyType;
+    using reference = Reference;
+};
 
-    template <typename T, typename Trait = property_map_trait<T>>
-    concept PropertyMap = requires(Trait t) {
-        typename Trait::value_type;
-        typename Trait::key_type;
-        typename Trait::reference;
-    };
+template <typename T>
+auto property_map_trait(T&&) -> property_map_types<typename T::value_type, typename T::key_type, typename T::reference>;
 
-    template <typename T, typename Trait = property_map_trait<T>>
-    concept ReadablePropertyMap =
-        PropertyMap<T> &&
-        requires (T map, typename Trait::key_type key) { { get(map, key) } -> std::same_as<typename Trait::reference>; };
 
-    template <typename T, typename Trait = property_map_trait<T>>
-    concept WriteablePropertyMap =
-        PropertyMap<T> && ReadablePropertyMap<T> &&
-        requires (T map, typename Trait::key_type key, typename Trait::value_type value) { { put(map, key, value) } -> std::same_as<void>; };
 
-    template <typename T, typename Trait = property_map_trait<T>>
-    concept LValuePropertyMap =
-        PropertyMap<T> && WriteablePropertyMap<T> &&
-        ( std::is_same_v<typename Trait::reference, typename Trait::value_type const&> || std::is_same_v<typename T::reference, typename T::value_type&> );
+template <typename T, typename Trait = decltype(property_map_trait(std::declval<T>()))>
+concept PropertyMap = requires(Trait t) {
+    typename Trait::value_type;
+    typename Trait::key_type;
+    typename Trait::reference;
+};
 
-  // CRTP base class that will provide freestanding put / get
-  // functions, when used as base class.
+template <typename T, typename Trait = decltype(property_map_trait(std::declval<T>()))>
+concept ReadablePropertyMap =
+    PropertyMap<T> &&
+    requires (T map, typename Trait::key_type key) { { get(map, key) } -> std::same_as<typename Trait::reference>; };
 
-  template <typename Map>
-  struct put_get_base { };
+template <typename T, typename Trait = decltype(property_map_trait(std::declval<T>()))>
+concept WriteablePropertyMap =
+    PropertyMap<T> && ReadablePropertyMap<T> &&
+    requires (T map, typename Trait::key_type key, typename Trait::value_type value) { { put(map, key, value) } -> std::same_as<void>; };
 
-  template <typename Map, typename Trait = property_map_trait<Map>>
-  typename Map::reference
-  get(const put_get_base<Map>& pa, typename Trait::key_type const& k) {
-    typename Trait::reference v = static_cast<const Map&>(pa)[k];
+template <typename T, typename Trait = decltype(property_map_trait(std::declval<T>()))>
+concept LValuePropertyMap =
+    PropertyMap<T> && WriteablePropertyMap<T> &&
+    ( std::is_same_v<typename Trait::reference, typename Trait::value_type const&> || std::is_same_v<typename T::reference, typename T::value_type&> );
+
+
+
+
+// CRTP base class that will provide freestanding put / get
+// functions, when used as base class.
+template <typename Map>
+struct put_get_base { };
+
+template <typename Map, typename Trait = decltype(property_map_trait(std::declval<Map>()))>
+typename Map::reference
+get(const put_get_base<Map>& map, typename Trait::key_type const& key) {
+    typename Trait::reference v = static_cast<const Map&>(map)[key];
     return v;
-  }
+}
 
-  template <typename Map, typename Trait = property_map_trait<Map>>
-  void
-  put(const put_get_base<Map>& pa, typename Trait::key_type const& k, typename Trait::value_type const& v) {
-    static_cast<const Map&>(pa)[k] = v;
-  }
+template <typename Map, typename Trait = decltype(property_map_trait(std::declval<Map>()))>
+void
+put(const put_get_base<Map>& map, typename Trait::key_type const& key, typename Trait::value_type const& value) {
+    static_cast<const Map&>(map)[key] = value;
+}
 
-  template <typename UniquePairAssociativeContainer, bool Const = false>
-  class associative_property_map : public put_get_base<associative_property_map<UniquePairAssociativeContainer, Const>> {
+
+
+template <typename UniquePairAssociativeContainer, bool Const = false>
+class associative_property_map : public put_get_base<associative_property_map<UniquePairAssociativeContainer, Const>> {
     using Container = UniquePairAssociativeContainer;
     std::conditional_t<Const, Container const*, Container*> _container;
 
-  public:
+public:
     using key_type = typename Container::key_type;
     using value_type = typename Container::value_type::second_type;
     using reference = std::conditional_t<Const, const value_type&, value_type&>;
 
     //associative_property_map() : m_c(0) { }
     associative_property_map(Container& c) : _container(&c) { }
+    associative_property_map(Container const& c) : _container(&c) { }
 
     reference operator[](const key_type& k) const {
         if constexpr(Const) {
@@ -81,8 +91,10 @@ namespace ext { namespace graph {
             return (*_container)[k];
         }
     }
+};
 
-  };
+template <typename T>
+associative_property_map(T const&) -> associative_property_map<T, true>;
 
 }}     // namespace ext::graph
 #endif
--- tests/test_property_map.cpp
+++ tests/test_property_map.cpp
@@ -7,11 +7,31 @@ template <ext::graph::LValuePropertyMap T>
       (void)h;
 }
 
-TEST(property_map, create) {
+TEST(property_maps, associative_property_map) {
+    {
+        auto map = std::map<int,int>();
+        ext::graph::associative_property_map pmap(map);
 
-  auto map = std::map<int,int>();
-  ext::graph::associative_property_map pmap(map);
+        testLValuePropertyConcept(pmap);
+        put(pmap,1,2);
+        auto actual = get(pmap,1);
+        ASSERT_EQ(2,actual);
+    }
 
-  testLValuePropertyConcept(pmap);
+    {
+        auto map = std::map<int,int>();
+        map[1] = 2;
+
+        auto const& cmap = map;
+        ext::graph::associative_property_map cpmap(cmap);
+
+        testLValuePropertyConcept(cpmap); // how can the constness be checked?
+        //put(cpmap,1,3); // fails to compile -- good
+
+        auto actual = get(cpmap,1);
+        ASSERT_EQ(2,actual);
+    }
+
+
 
 }
